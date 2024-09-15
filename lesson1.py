9edde84c05e872bb7286f7a60ee95cf3d94a62e6
gabs-vzqz <vazquez.g.gabs@gmail.com>
2024-09-14 23:40:39 -0700

    lesson1

---
+++ lesson1.py
@@ -0,0 +1,37 @@
+# REMINDER: When running python on terminal, start with python3 for mac. Use quit() to exit
+
+name = 'Gaby' 
+# ^this is a VARIABLE, the variable is "name" and it hold the VALUE "Gaby"
+print(name) #this command will now print as "Gaby" in the terminal
+
+
+
+line05 = "************************"
+line06 = "*                      *"
+line07 = "*        WELCOME       *"
+
+print(line05)
+print(line06)
+print(line07)
+print(line05)
+
+# ****PYTHON OPERATORS - symbols used to preform operations on values and the variable that hold these values
+
+name = 'Gaby'
+# ^Assignment operator is the = sign
+
+print(2+2)
+#^ The + is the addition operator
+
+print(5/2)
+# ^ Division operator
+
+print(2*5)
+#^ Multiplication Operator 
+
+print(10-5)
+# ^ subtraction operator 
+
+print(50//30)
+#^ For division, id will round the answer DOWN, no decimal
+
